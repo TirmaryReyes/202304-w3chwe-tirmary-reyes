b0380d51d3cb3bb4a2c1d83f8c18d8df418a5e1a
Tirmary Reyes <tmrv1991@gmail.com>
2023-04-22 19:48:20 +0200

    Add component card information

--- src/ts/components/Component/Component.ts
+++ src/ts/components/Component/Component.ts
@@ -1,4 +1,4 @@
-import type ComponentStructure from "../types.js";
+import type ComponentStructure from "../types";
 
 abstract class Component implements ComponentStructure {
   domElement: HTMLElement;
